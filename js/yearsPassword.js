3e7593ac662fd08805941f1bf9f74b48a55e6cc7
JosivalSilva <silva.josival@yahoo.com>
2022-11-26 18:56:57 -0300

    Implementação de visualizador de senhas nas telas de Login e Cadastro

--- js/yearsPassword.js
+++ js/yearsPassword.js
@@ -1,6 +1,9 @@
 let olhos = document.getElementById("olhos");
+let olhosConfirma = document.getElementById("olhosConfirma");
 let flag = false;
 let tipoSenha = document.getElementById('inputSenha')
+let tipoConfirmaSenha = document.getElementById('inputConfirmaSenha')
+
 olhos.addEventListener("click", (evento =>{
   if (flag == false){
   flag = true
@@ -11,5 +14,18 @@ olhos.addEventListener("click", (evento =>{
     flag = false
     olhos.src = '../Mobile/Header Imagens/olho fechado.png';
     tipoSenha.type = 'password'
+  }
+}))
+
+olhosConfirma.addEventListener("click", (evento =>{
+  if (flag == false){
+  flag = true
+  olhosConfirma.src = '../Mobile/Header Imagens/olhoaberto.png';
+  tipoConfirmaSenha.type = 'text'
+  }
+  else{
+    flag = false
+    olhosConfirma.src = '../Mobile/Header Imagens/olho fechado.png';
+    tipoConfirmaSenha.type = 'password'
   }
 }))
